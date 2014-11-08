78b10c9212f01b85ececc9d04000c18b12160e65
scvishnu7 <bidaribishnu7@gmail.com>
2014-11-08 21:32:59 +0545

    search and sort div added

--- admin.php
+++ admin.php
@@ -6,7 +6,7 @@
 	include('dbMgmt.php');
 	
 	$db = new dbManager();
-	//$suggestions = new Array();
+	$suggestions = array();
 	$db->init_db();
 	$suggestions = $db->getAllSuggestion();
 ?>
@@ -18,16 +18,25 @@
 	}
 
 .evenRow {
-	background-color: green;
+	background-color: white;
 }
-
+#containerDiv {
+	padding-left:20px;
+	padding-right:20px;
+	padding-top:10px;
+	padding-bottom:10px;
+	background-color:gray;
+	width:60%;
+	margin:auto;
+	align:center;
+}
 #suggestionDiv {
 	padding-left:20px;
 	padding-right:20px;
 	padding-top:10px;
 	padding-bottom:10px;
 	background-color:pink;
-	width:60%;
+	width:90%;
 	margin:auto;
 	align:center;
 }
@@ -59,6 +68,19 @@ td {
 We need to add the sorting, grouping by the issues and the people tagged. Also the searching feature.<br/>
 Thinking about deleting feature. But i think it would be better to put delete button somewhere in remote :D.
 </p>
+
+<div id="containerDiv">
+<div id="sortOptions">
+<form action="#" method="POST">
+List suggestions On : <input type="text" name="issues">
+tagged to :<input type="text" name="personTagged">
+in period :<input type="text" name="period">
+containing word: <input type="text" name="searchKeyword">
+<input type="submit" name="list" value="Ok">
+</form>
+</div>
+<hr/>
+
 <div  id="suggestionDiv">
 <table  >
 	<tr>
@@ -84,6 +106,8 @@ TABLE_ROW;
 ?>
 
 </table>
+
+</div>
 
 </div>
 
